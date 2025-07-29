c44ed67458d7810350b8c5fe9a1ff7210876b8d0
Anuradha Goel <121583764+Anuradhagoel@users.noreply.github.com>
2025-07-29 08:24:45 -0500

    Update import.py

--- import.py
+++ import.py
@@ -115,7 +115,7 @@ def get_conversation_chain(vectorstore, conversational_model, model_temperature=
     elif conversational_model == "HuggingFace":
         st.info("Using HuggingFace conversational model (via pipeline).")
         # USING THE SMALLEST FLAN-T5 MODEL
-        model_id = "google/flan-t5-xxl"
+        model_id = "google/flan-t5-small"
         
         try:
             tokenizer = AutoTokenizer.from_pretrained(model_id)
